49c7f4379f8f9d9a8838305388af7b0ce5ab1863
Nigel Gomes <nigelgomesdeveloper@gmail.com>
2020-08-08 18:21:08 -0500

    # arrow functions & fetch API samples...

---
+++ sandbox/section_7/arrow_functions/app.js
@@ -0,0 +1,13 @@
+
+// const sayHello = name => console.log(`Hello ${name}`);
+// sayHello('John');
+
+// const sayHello = (firstName, lastName) => console.log(`Hello ${firstName} ${lastName}`);
+// sayHello('John', 'Doe');
+
+//  const getUser =  () => ({ firstName: 'John', lastName: 'Doe' })
+//  console.log(getUser())
+
+names = ['John', 'Jane', 'Shane'];
+const getNameLengths = names.map(name => name.length);
+console.log(getNameLengths);
--- sandbox/section_7/fetch_api/app.js
+++ sandbox/section_7/fetch_api/app.js
@@ -1,60 +1,54 @@
-document.getElementById('button1').addEventListener('click', getText);
-document.getElementById('button2').addEventListener('click', getJSON);
-document.getElementById('button3').addEventListener('click', getExternal);
-
-function getText() {
+const getText = () => {
   fetch('text.txt')
-    .then(function(response) {
-      return response.text();
-    })
-    .then(function(text) {
-      document.getElementById('output').innerHTML = text;
-    })
-    .catch(function(error) {
-      console.error(error)
-    })
+    .then(response => response.text()
+    )
+    .then(text => document.getElementById('output').innerHTML = text
+    )
+    .catch(error => console.error(error)
+    )
 }
 
-function getJSON() {
+const getJSON = () => {
   fetch('post.json')
     .then(handleNetworkError)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(json) {
+    .then(response => response.json()
+    )
+    .then(json => {
       let output = '';
 
-      json.forEach(function(post) {
+      json.forEach(post => {
         output += `<li>${post.title}</li>`;
       })
       document.getElementById('output').innerHTML = output;
     })
-    .catch(function(error) {
-      console.warn(error)
-    })
+    .catch(error => console.warn(error)
+    )
 }
 
-function getExternal() {
+const getExternal = () => {
   fetch('https://api.github.com/users')
     .then(handleNetworkError)
-    .then(function(response) {
+    .then(response => {
       return response.json();
     })
-    .then(function(json) {
+    .then(json => {
       let output = '';
 
-      json.forEach(function(user) {
+      json.forEach(user => {
         output += `<li>${user.login}</li>`;
       })
       document.getElementById('output').innerHTML = output;
     })
-    .catch(function(error) {
-      console.warn(error)
-    })
+    .catch(error => console.warn(error)
+    )
 }
 
-function handleNetworkError(response) {
+handleNetworkError = response => {
   if (!response.ok) throw new Error(response.error);
 
   return response;
 }
+
+document.getElementById('button1').addEventListener('click', getText);
+document.getElementById('button2').addEventListener('click', getJSON);
+document.getElementById('button3').addEventListener('click', getExternal);
